8b91b6848b4585cd50e197316df2e5995e25306c
Andrew-Hany <andrewhany@aucegypt.edu>
2020-09-17 12:57:08 +0200

    Updates

---
+++ precache-manifest.513abacc32b12b8e4594c48a717f15cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280ed3748b31031c088718b9354baa2b",
+    "url": "/Thanawy_Marimena/index.html"
+  },
+  {
+    "revision": "bfddececd12db3504321",
+    "url": "/Thanawy_Marimena/static/css/main.c59cfe11.chunk.css"
+  },
+  {
+    "revision": "a7fe49a02eb021f6f2bc",
+    "url": "/Thanawy_Marimena/static/js/2.77b69150.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/Thanawy_Marimena/static/js/2.77b69150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfddececd12db3504321",
+    "url": "/Thanawy_Marimena/static/js/main.e49bd9e3.chunk.js"
+  },
+  {
+    "revision": "185a0b8c573e0f576e0d",
+    "url": "/Thanawy_Marimena/static/js/runtime-main.5a88b260.js"
+  },
+  {
+    "revision": "639bea94f2929c06508481216851a65c",
+    "url": "/Thanawy_Marimena/static/media/logo.639bea94.jpeg"
+  },
+  {
+    "revision": "a17523f59609e5fbb8d96166285dbfd1",
+    "url": "/Thanawy_Marimena/static/media/marmina.a17523f5.jpg"
+  },
+  {
+    "revision": "bc313e2a2be9cc5e92958072cc564ae4",
+    "url": "/Thanawy_Marimena/static/media/video.bc313e2a.mp4"
+  }
+]);
