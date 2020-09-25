1cc0590801df6243e7322eea26dc030b6ac79dd4
Andrew-Hany <andrewhany@aucegypt.edu>
2020-09-26 00:05:52 +0200

    bible

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import './circle.css';
 class App extends Component {
   state = {
     //face 
-    auth: false,
+    auth: true,
     name: '',
     picture: '',
     mails: '',
--- src/components/2s7a7.jsx
+++ src/components/2s7a7.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-
+import './2s7a7.css';
 export default class questions extends Component {
+
+    state = {
+
+        Oncomment: false,
+
+
+    }
+    toggleComment = () => {
+        this.setState({
+            Oncomment: true,
+        })
+    }
     render() {
         return (
-            <div className="container">
+            <div className="as7a7">
 
-                <h1 style={{ textAlign: "center", margin: "50px" }}>{this.props.as7a7.name}</h1>
-                <p className="badge-light rounded" style={{ textAlign: "right", padding: "40px" }}>{this.props.content}</p>
-                <div className="row">
-                    <div className="col-9">
+                <h1 className="header">{this.props.as7a7.name}</h1>
+                <p className="badge-light rounded as7a7Content" >{this.props.content}</p>
+                <div >
+                    <div >
                         {
 
-                            !this.props.read ? <button className="bg-success rounded col-2  p-2 " onClick={() => this.props.handleread(this.props.as7a7)}>finish reading</button> :
+                            !this.props.read ? <button className="bg-success rounded   p-2 " onClick={() => this.props.handleread(this.props.as7a7)}>finish reading</button> :
                                 <div >
                                     <h2>finished, check the progress Bar in the home page</h2>
-                                    <button className="bg-success rounded col-2  p-2" onClick={() => this.props.toggleonHome()}>return to home </button>
+                                    <button className="bg-success rounded  p-2" onClick={() => this.props.toggleonHome()}>return to home </button>
+
                                 </div>
+                        }
+                        {!this.state.Oncomment && <a className="bg-light comment " onClick={() => this.toggleComment()}>comment </a>}
+                        {this.state.Oncomment &&
+
+                            <a className="bg-light comment " onClick={() => this.toggleComment()}>comment </a>
+
                         }
                     </div>
                 </div>
